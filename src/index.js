a1a8d1d42ce9cf3f9b2a5211fe02cab56fa4de55
Ameet Kaustav <akaustav@users.noreply.github.com>
2019-10-03 00:17:31 -0700

    [+] Observer and subscribe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 const { of } = require('rxjs');
 
 const observable = of(1, 2, 3);
-console.log(observable);
+
+const observer = {
+  next: x => {
+    console.log(x);
+  }
+};
+
+observable.subscribe(observer);
